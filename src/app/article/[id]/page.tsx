d842e374173e6d60d456493177a4abf6de756b37
Arman Maurya <armanmarya6@gmail.com>
2024-06-15 10:54:31 +0530

    Replace the React-markdown renderer in article/page.tsx to Slate-Custom Renderer which renderers the slate document on server-side

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,69 +1,63 @@
-import Markdown, { Components } from "react-markdown";
-import remarkBreaks from "remark-breaks";
-
-import rehypeKatex from "rehype-katex";
-import remarkMath from "remark-math";
-import "katex/dist/katex.min.css"; // `rehype-katex` does not import the CSS for you
-
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { oneLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import React, { useCallback } from "react";
+import {
+  H1Element,
+  H2Element,
+  H3Element,
+  H4Element,
+  H5Element,
+  H6Element,
+  CodeElement,
+  ImageElement,
+  DefaultElement,
+  Leaf,
+  // Leaf,
+} from "@/app/components/slate/element";
 import { API_ENDPOINT, NodeType } from "@/app/utils";
-import { SlateToMarkdown } from "@/app/components/slate/utils";
+import { Descendant } from "slate";
+import { RenderElementProps, RenderLeafProps } from "slate-react";
+import { CustomElement } from "@/app/components/slate/editor";
 
-// import { H1Element, H2Element, H3Element, H4Element, H5Element, H6Element } from "@/app/components/CustomElements";
-
-const components: Components = {
-  h1: ({ node, ...props }) => (
-    <h1 className="text-4xl font-bold text-black" {...props} />
-  ),
-  h2: ({ node, ...props }) => (
-    <h2 className="text-3xl font-semibold text-black" {...props} />
-  ),
-  h3: ({ node, ...props }) => (
-    <h3 className="text-2xl font-semibold text-black" {...props} />
-  ),
-  h4: ({ node, ...props }) => (
-    <h4 className="text-xl font-semibold text-black" {...props} />
-  ),
-  h5: ({ node, ...props }) => (
-    <h5 className="text-lg font-semibold text-black" {...props} />
-  ),
-  h6: ({ node, ...props }) => (
-    <h6 className="text-base font-semibold text-black" {...props} />
-  ),
-  p: ({ node, ...props }) => (
-    <p className="text-base py-1" {...props}>
-      {props.children}
-    </p>
-  ),
-
-  code: ({ node, inline, className, children, ...props }: any) => {
-    const match = /language-(\w+)/.exec(className || "");
-
-    return !inline && match ? (
-      <SyntaxHighlighter
-        style={oneLight}
-        PreTag="div"
-        language={match[1]}
-        {...props}
-      >
-        {String(children).replace(/\n$/, "")}
-      </SyntaxHighlighter>
-    ) : (
-      <code style={{ borderRadius: "0.5rem", backgroundColor: "#e0e0e0" , padding:"3px"}} className={className} {...props}>
-        {children}
-      </code>
-    );
-    // return (
-    //   <code
-    //     style={{ borderRadius: "0.5rem", backgroundColor: "#e0e0e0" }}
-    //     className=""
-    //     {...props}
-    //   />
-    // );
-  },
-  pre: ({ node, ...props }) => <pre className="my-1" {...props} />,
+// Define a custom Element component for rendering
+const Element = (props: RenderElementProps) => {
+  const { attributes, children, element } = props;
+  switch (element.type) {
+    case NodeType.H1:
+      return <H1Element {...props} />;
+    case NodeType.H2:
+      return <H2Element {...props} />;
+    case NodeType.H3:
+      return <H3Element {...props} />;
+    case NodeType.H4:
+      return <H4Element {...props} />;
+    case NodeType.H5:
+      return <H5Element {...props} />;
+    case NodeType.H6:
+      return <H6Element {...props} />;
+    case NodeType.CODE:
+      return <CodeElement {...props} />;
+    case "image":
+      return <ImageElement {...props} />;
+    default:
+      return <DefaultElement {...props} />;
+  }
 };
+
+const ServerLeaf = (props: RenderLeafProps) => {
+  return <Leaf {...props} />;
+};
+
+async function getData(id: string): Promise<Article> {
+  try {
+    const res = await fetch(`${API_ENDPOINT.article.url}?id=${id}`);
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error; // Rethrow the error to propagate it to the caller
+  }
+}
 
 interface Article {
   id: number;
@@ -73,45 +67,39 @@ interface Article {
   created_at: string;
 }
 
-async function getData(id: string) {
-  try {
-    const res = await fetch(
-      `${API_ENDPOINT.article.url}?id=${id}`
-    );
-    return res.json();
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    // Handle the error here, e.g. show an error message to the user
-    throw error; // Rethrow the error to propagate it to the caller
-  }
-}
 const Page = async ({ params: { id } }: { params: { id: string } }) => {
-  const data: Promise<Article> = await getData(id);
+  const data: Article = await getData(id);
 
-  const content = (await data).content;
-  // console.log(typeof content);
-  const jsonContent = JSON.parse(content);
-  
-  const slateToMarkdown = new SlateToMarkdown(NodeType);
-  const markdown = slateToMarkdown.convert(jsonContent);
-  // console.log(markdown);
-  // await new Promise((resolve) => setTimeout(resolve, 3000));  // Add Wait of 3 seconds
-
+  const content = data.content;
+  // const content = `[{"type":"heading-one","align":"center","children":[{"text":"Heading One"}]},{"type":"heading-one","align":"center","children":[{"text":"Heading One"}]},{"type":"heading-two","align":"left","children":[{"text":"Heading Two"}]},{"type":"paragraph","align":"center","children":[{"text":"A line of text in a paragraph."},{"text":"Bold","bold":true},{"text":" Some More Text "},{"text":"Italic","italic":true},{"text":" Some more text "},{"text":"Underlined","underline":true},{"text":" Some more text "},{"text":"Bold Italic","bold":true,"italic":true}]},{"type":"heading-three","align":"right","children":[{"text":"Heading Three"}]},{"type":"heading-four","align":"right","children":[{"text":"Heading Four"}]},{"type":"heading-five","align":"center","children":[{"text":"Heading Five"}]},{"type":"heading-six","align":"center","children":[{"text":"Heading Six"}]},{"type":"code","language":"javascript","children":[{"text":"const a = 5;"},{"text":"const b = 10;"}]},{"type":"paragraph","align":"right","children":[{"text":"A line of text \nin a paragraph."},{"text":"Bold","bold":true},{"text":" Some More Text "},{"text":"Italic","italic":true},{"text":" Some more text "},{"text":"Underlined","underline":true},{"text":" Some more text "},{"text":"Bold Italic","bold":true,"italic":true}]}]`;
+  const jsonContent: CustomElement[] = JSON.parse(content);
   return (
     <div className="">
       <div className="flex items-center justify-center p-2">
         <h1 className="text-4xl font-semibold">
-          <u>{(await data).title}</u>
+          <u>{data.title}</u>
         </h1>
       </div>
-      <Markdown
-        className="p-4 pb-10"
-        components={components}
-        remarkPlugins={[remarkMath, remarkBreaks]}
-        rehypePlugins={[rehypeKatex]}
-      >
-        {markdown}
-      </Markdown>
+      <div className="px-4">
+        {jsonContent.map((node, i) => (
+          <Element
+            key={i}
+            element={node}
+            attributes={{
+              "data-slate-node": "element",
+              ref: null,
+            }}
+          >
+            {node.children.map((leaf, j) => (
+              <ServerLeaf key={j} leaf={leaf} text={leaf} attributes={{
+                'data-slate-leaf': true,
+              }}>
+                {leaf.text}
+              </ServerLeaf>
+            ))}
+          </Element>
+        ))}
+      </div>
     </div>
   );
 };
