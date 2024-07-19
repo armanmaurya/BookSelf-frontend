555394ca1e4880260cfc0ec25978577cce1c3b07
Arman Maurya <armanmarya6@gmail.com>
2024-07-20 00:02:33 +0530

    Add a ui package

--- apps/web/src/app/about/page.tsx
+++ apps/web/src/app/about/page.tsx
@@ -1,9 +1,12 @@
-'use client'
-import { Bars } from "react-loader-spinner"
+"use client";
+import { Bars } from "react-loader-spinner";
+import { MyButton } from "@repo/ui";
+
 export default function Loading() {
-    return (
-      <div className="flex items-center justify-center bg-slate-600 h-full">
-        <Bars color="#000" height={80} width={80} />
-      </div>
-    )
+  return (
+    <div className="flex items-center justify-center bg-slate-600 h-full">
+      <Bars color="#000" height={80} width={80} />
+      <MyButton/>
+    </div>
+  );
 }
