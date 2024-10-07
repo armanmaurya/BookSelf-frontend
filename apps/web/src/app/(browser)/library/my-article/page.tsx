0cf0400505b62ec8a6c30ee4730a54254efa0aa9
Arman Maurya <armanmarya@gmail.com>
2024-10-07 23:37:09 +0530

    Added Button to Delete Article

--- apps/web/src/app/(browser)/library/my-article/page.tsx
+++ apps/web/src/app/(browser)/library/my-article/page.tsx
@@ -4,23 +4,25 @@ import { ArticleCard } from "@/components/blocks";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import React from "react";
+import { DeleteButton } from "./button";
 
 const fetchData = async () => {
   const cookieStore = cookies();
 
   const res = await fetch(`${API_ENDPOINT.article.url}myarticles/`, {
     headers: {
-      Cookie: `${cookieStore.get("sessionid")?.name}=${cookieStore.get("sessionid")?.value
-        }`,
+      Cookie: `${cookieStore.get("sessionid")?.name}=${
+        cookieStore.get("sessionid")?.value
+      }`,
     },
   });
   if (!res.ok) {
     return null;
   }
   return res.json();
-
-
 };
+
+
 
 const Page = async () => {
   const data: Promise<Article>[] = await fetchData();
@@ -32,10 +34,23 @@ const Page = async () => {
     <div className="w-full h-full space-y-2 overflow-auto flex flex-col p-2">
       {data.map(async (articlePromise) => {
         const article = await articlePromise;
-        return <ArticleCard key={article.id} data={article} />;
+        return (
+          <div className="w-full flex flex-col overflow-auto relative">
+            {" "}
+            <DeleteButton slug={article.slug}/>
+            <ArticleCard key={article.id} data={article} />
+          </div>
+        );
       })}
     </div>
   );
 };
 
 export default Page;
+
+{
+  /* <div className="relative">
+          <button className="bg-red-400 rounded-md p-2 absolute right-2 top-2">Delete</button>
+          <ArticleCard key={article.id} data={article} />
+        </div>; */
+}
